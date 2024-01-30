2917b53ffd6c6a52c17839e39d0988eeb491ae19
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-01-30 02:47:13 +0200

    bannerallpages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Product from './Pages/Product';
 import Card from './Pages/Card';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import men_banner from "./Components/Assets/banner_mens.png"
+import women_banner from "./Components/Assets/banner_women.png"
+import kids_banner from "./Components/Assets/banner_kids.png"
 
 function App() {
   return (
@@ -15,9 +18,9 @@ function App() {
             <NavBar/>
             <Routes>
               <Route path='/' element={<Shop/>}/>
-              <Route path='/mens' element={<ShopCategort  category="men"/>}/>
-              <Route path='/womens' element={<ShopCategort category="women"/>}/>
-              <Route path='/kids' element={<ShopCategort category="kid"/>}/>
+              <Route path='/mens' element={<ShopCategort banner={men_banner}  category="men"/>}/>
+              <Route path='/womens' element={<ShopCategort banner={women_banner} category="women"/>}/>
+              <Route path='/kids' element={<ShopCategort  banner={kids_banner} category="kid"/>}/>
               <Route path='/product' element={<Product/>}>
                 <Route path=':productId' element={<Product/>}/>
               </Route>
