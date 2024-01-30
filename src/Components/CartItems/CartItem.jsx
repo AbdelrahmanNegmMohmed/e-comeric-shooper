5233dee5983c90a24d3a077dd3f69fc0f255b814
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-01-30 11:42:39 +0200

    pop error

--- src/Components/CartItems/CartItem.jsx
+++ src/Components/CartItems/CartItem.jsx
@@ -16,26 +16,20 @@ const CartItem = () => {
             <p>Remove</p>
         </div>
         <hr />
-      {all_product.map((item)=>{
-        if(CartItem[item.id]>0) 
-        
+      {all_product.map((ltem)=>{
+        if(CartItem[ltem.id]>0)        
         return<div>
         <div className="cartitems-format">
-            <img src={item.image} alt="" className='carticon-product-icon' />
-            <p>{item.name}</p>
-            <p>{item.new_price}</p>
-            <button className='cartitem-contity'>{CartItem[item.id]}</button>
-            <p>{item.new_price*CartItem[item.id]}</p>
-            <img src={remove_icon} onClick={()=>{removefromcart(item.id)}} alt="" />
+            <img src={ltem.image} alt="" className='carticon-product-icon' />
+            <p>{ltem.name}</p>
+            <p>{ltem.new_price}</p>
+            <button className='cartitem-contity'>{CartItem[ltem.id]}</button>
+            <p>{ltem.new_price*CartItem[ltem.id]}</p>
+            <img src={remove_icon} onClick={()=>{removefromcart(ltem.id)}} alt="" />
         </div>
         <hr />
     </div>
-
-
       })}
-        
-      
-
     </div>
   )
 }
